bb547cd0190a62f5378f09d8877b9a59decb416b
Nader wael <naderwael39@gmail.com>
2026-06-06 18:38:27 +0300

    state classes

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -4,4 +4,11 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterInitial());
+  void increment() {
+    emit(CounterUpdate(state.count + 1));
+  }
+
+  void decrement() {
+    emit(CounterUpdate(state.count - 1));
+  }
 }
