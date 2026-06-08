ebaab62a4a2a89da61d57b4bfa8384c9650f3c07
Nader wael <naderwael39@gmail.com>
2026-06-08 19:39:17 +0300

    make comment to describe

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:meta/meta.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'counter_state.dart';
+//tow functions increment and decrement will be called by the UI widget and the state will be updated by the cubit
+//cubit will emit the new state and the UI widget will rebuild with the new state
 
 class CounterCubit extends Cubit<CounterState> {
+  //CounterCubit() : super(CounterInitial()); take no parameters and the initial state is CounterInitial"
   CounterCubit() : super(CounterInitial());
   void increment() {
     emit(CounterUpdate(state.count + 1));
--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -3,13 +3,13 @@ part of 'counter_cubit.dart';
 @immutable
 sealed class CounterState {
   final int count;
-  CounterState(this.count);
+  const CounterState(this.count);
 }
 
 final class CounterInitial extends CounterState {
-  CounterInitial() : super(0);
+  const CounterInitial() : super(0);
 }
 
 class CounterUpdate extends CounterState {
-  CounterUpdate(super.count);
+  const CounterUpdate(super.count);
 }
